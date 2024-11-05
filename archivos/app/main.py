b4ee583c6f47d43eeac9642e0ff5411172a045a3
Faabrii <fabrimartinez66@gmail.com>
2024-11-04 22:05:21 -0300

    Agregamos la clase de pandas al repo, todo funciona bien

--- archivos/app/main.py
+++ archivos/app/main.py
@@ -1,15 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('./data.csv') # leemos el csv, gracias al módulo línea 2
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  '''
+  
+  df = pd.read_csv ("data.csv") #Estamos leyendo el archivo csv
+  df = df[df["Continent"] == "Africa"]
+  countries = df["Country"].values
+  percentages = df["World Population Percentage"].values
+  charts.generate_pie_chart(countries, percentages)
 
+  data = read_csv.read_csv('./data.csv')
   country = input('Type Country => ')
   print(country)
 
